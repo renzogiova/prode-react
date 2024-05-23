643c2d6fdaddc9e48fc6c0a656a11e1e07578b71
renzo giova <rgiova.cs@gmail.com>
2024-05-23 20:00:32 -0300

    some very useful style changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import AuthTabs from './components/AuthTabs/AuthTabs';
 import Home from './components/Home/Home';
 
-import './App.css'
 import Clasificacion from './components/Clasificacion/Clasificacion';
+
+import './App.css'
 
 function App() {
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,8 +9,9 @@ function Home() {
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   }
-  return (
-      <Container fluid style={{height: '100vh', padding: 0}}>
+  return (          
+      // <Container fluid style={{minHeight: '100vh', height: '100%', padding: 0}}>
+      <Container fluid style={{height: '100%', padding: 0}}>
         <Row style={{height: '100%', width: '100%', margin:0}}>
           {/* Sidebar COL */}
           <Col xs={2} style={{padding:0}}>
@@ -32,16 +33,18 @@ function Home() {
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta aliquam expedita earum saepe, omnis, eos aperiam suscipit at laboriosam qui ipsa commodi amet tenetur cum dolores aliquid quia mollitia facere nisi incidunt excepturi vero molestias. In, a repellendus! Quia, officiis. Ipsa illo corporis doloremque adipisci explicabo, ipsam sapiente nulla exercitationem.
                   </p>
                 </div>
-                <div>
+                {/* <div>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis perspiciatis soluta, asperiores provident earum natus eveniet possimus ab tempora tempore eligendi, quo distinctio et! Placeat repudiandae nostrum natus deleniti ea qui vel iure explicabo quaerat, eaque ad dolores aspernatur pariatur cum veritatis nisi animi quisquam! Sapiente sit exercitationem quod? Sunt laudantium atque corrupti voluptates nisi eos, dolores nobis vel corporis, iste cumque quae rerum voluptate, tempore sit sequi saepe labore quidem rem dolorum magni minus. Itaque nostrum iste autem ipsum exercitationem. Dolorum, numquam! Cumque non doloremque voluptas asperiores ipsa quis omnis! Nisi nesciunt commodi consequuntur. Odit cumque natus sapiente obcaecati minus expedita doloribus laborum qui suscipit nam delectus iusto, eius aliquid labore animi sequi, facilis veniam blanditiis ea dolores!</p>
                 </div>
                 <div>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis perspiciatis soluta, asperiores provident earum natus eveniet possimus ab tempora tempore eligendi, quo distinctio et! Placeat repudiandae nostrum natus deleniti ea qui vel iure explicabo quaerat, eaque ad dolores aspernatur pariatur cum veritatis nisi animi quisquam! Sapiente sit exercitationem quod? Sunt laudantium atque corrupti voluptates nisi eos, dolores nobis vel corporis, iste cumque quae rerum voluptate, tempore sit sequi saepe labore quidem rem dolorum magni minus. Itaque nostrum iste autem ipsum exercitationem. Dolorum, numquam! Cumque non doloremque voluptas asperiores ipsa quis omnis! Nisi nesciunt commodi consequuntur. Odit cumque natus sapiente obcaecati minus expedita doloribus laborum qui suscipit nam delectus iusto, eius aliquid labore animi sequi, facilis veniam blanditiis ea dolores!</p>
-                </div>
+                </div> */}
               </div>
             </main>
-            <Footer/>          
           </Col>
+        </Row>
+        <Row style={{padding: 0, margin:0}}>
+          <Footer/>          
         </Row>
       </Container>
     // </div>
